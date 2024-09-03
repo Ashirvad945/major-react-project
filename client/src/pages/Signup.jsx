2b8ef88d311abfddd2d6620bdfc5d7ce4b54f57d
Ashirvad945 <ashirvadpatel86@gmail.com>
2024-09-03 05:35:51 -0700

    error occured . page is not navigate to another page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,51 +1,137 @@
-import {Link} from "react-router-dom"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Signup = () => {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [accountType, setAccountType] = useState("buyer");
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(import.meta.env.VITE_API_URL + "/signup", {
+        username,
+        email,
+        password,
+        accountType,
+      });
+      const data = await res.data;
+      if (data.success === true) {
+        setUsername("");
+        setEmail("");
+        setPassword("");
+        setAccountType("");
+        toast.success(data.message);
+        navigate("/login");
+      }
+      console.log(data);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
   return (
-    <div className="mt-20 sm:mt-20 min-h-screen flex items-center justify-center w-full">
+    <div className="mt-20 sm:mt-10 min-h-screen flex items-center justify-center w-full ">
       <div className="bg-white shadow-md rounded-3xl px-5 py-6 w-full sm:w-[27vw]">
-        <h1 className="text-2xl font-bold text-center mb-4">Let's Connects!</h1>
-        <form >
-        {/* for user name */}
+        <h1 className="text-2xl font-bold text-center mb-4">Let's Connect!</h1>
+        <form onSubmit={handleSignup}>
+          {/* For username */}
           <div className="mb-4">
-            <label htmlFor="username" className="block text-sm font-medium text-gray-700 mb-2">Username</label>
-            <input type="text" name="name" id="name" placeholder="ap945" className="shadow-md rounded-md w-full px-3 py-3 border border-gray-300 focus:outline-none
-            focus:ring-black focus:border-black" />
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+              placeholder="coder29"
+              className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            />
           </div>
 
-{/* for email */}
+          {/* For email */}
           <div className="mb-4">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-2">Email Address</label>
-            <input type="text" name="email" id="email" placeholder="your@email.com" className="shadow-md rounded-md w-full px-3 py-3 border border-gray-300 focus:outline-none
-            focus:ring-black focus:border-black" />
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Email Address
+            </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="your@email.com"
+              className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            />
           </div>
-{/* for password */}
+
+          {/* For password */}
           <div className="mb-4">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-2">Password</label>
-            <input type="password" name="password" id="password" placeholder="Enter Your Password" className="shadow-md rounded-md w-full px-3 py-3 border border-gray-300 focus:outline-none
-            focus:ring-black focus:border-black" />
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+              className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            />
           </div>
-          {/*for account selection */}
+
+          {/* For account selection */}
           <div className="mb-4">
-           <label htmlFor="accountType" className="block text-sm font-medium text-gray-700 mb-2">Select Your Account Type</label>
-           <select className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none
-           focus:ring-black focus:border-black">
-            <option value="buyer">Buyer</option>
-            <option value="Seller">Seller</option>
-           </select>
+            <label
+              htmlFor="accountType"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Select Your Accont Type
+            </label>
+            <select
+              onChange={(e) => setAccountType(e.target.value)}
+              className="shadow-md rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-black focus:border-black"
+            >
+              <option value="buyer">Buyer</option>
+              <option value="seller">Seller</option>
+            </select>
           </div>
 
           {/* Login with account */}
           <div className="flex items-center justify-end mb-4">
-          <Link className="text-xs text-black hover:text-gray-600" to="/login">Logn In With Account</Link>
+            <Link className="text-xs text-black " to="/login">
+              Log In With Account
+            </Link>
           </div>
-          <button type="submit" className="w-full py-2 px-4 rounded-md shadow-md text-sm font-medium text-white bg-black ">
+
+          <button
+            type="submit"
+            className="w-full py-2 px-4 rounded-md shadow-md text-sm font-medium text-white bg-black "
+          >
             Signup
           </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
