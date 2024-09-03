36e0cbc6ca4de160011b8efe5b233ace772590b7
Ashirvad945 <ashirvadpatel86@gmail.com>
2024-09-03 00:12:12 -0700

    verify tokken

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require("../models/User")
 const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const { generateAccessToken } = require("../helpers/accessToken");
+const { generateRefreshToken } = require("./refreshToken");
 
 
 
@@ -25,6 +27,8 @@ const signup = async (req,res)=>{
       accountType,
      });
 
+     await user.save();
+
      return res
      .status(201)
      .json({sucess:true,message:"User created succesfully"});
@@ -34,7 +38,42 @@ const signup = async (req,res)=>{
 
 };
 
-const login = async (req,res)=>{};
+const login = async (req,res)=>{
+   const { email, password } = req.body;
+
+   try{
+      let user = await User.findOne({email});
+      if(!user){
+         return res.status(400).json({success:false,message:"Please signup"});
+      }
+      const comparePassword = await bcrypt.compare(password,user.password);
+      if(!comparePassword)
+         return res
+      .status(400)
+      .json({success:false,message:"Invalid credentials"});
+
+      const data = {
+         id: user._id,
+         accountType: user.accountType,
+         author: user.username,
+      };
+
+      const accessToken = generateAccessToken(data);
+      const refreshToken = generateRefreshToken(data);
+
+      return res.status(200).json({
+         success:true,
+         message:"Login successful",
+         accessToken,
+         refreshToken,
+         role:user.accountType,
+         author: user.username,
+      });
+
+   }catch(error){
+      return res.status(500).json({success:false,message:error.message});
+   }
+};
 
 
 module.exports = {login , signup};
