974881a8b9793ea03736b7c4ece5e08aaa94fa5e
Ashirvad945 <ashirvadpatel86@gmail.com>
2024-09-05 03:28:53 -0700

    making progress and dynamic image added feature

--- client/src/pages/SellerDashboard.jsx
+++ client/src/pages/SellerDashboard.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import DashboardSidebar from '../components/DashboardSidebar'
+import PhotoManagment from '../components/seller/PhotoManagment'
 
 const SellerDashboard = () => {
   return (
-    <div>
+    <div className='flex flex-col sm:flex-row'>
       <DashboardSidebar/>
       <div>
         {/* we will change the pages switch case */}
       </div>
+      <PhotoManagment/>
     </div>
   )
 }
